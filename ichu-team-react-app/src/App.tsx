6627fdd3f72a625d378f7a5b023bdd7a090ed722
Eduardo Guanco <eduardo.guanco@kdsoft.io>
2020-11-29 02:16:43 -0300

    refactor a component home

--- ichu-team-react-app/src/App.tsx
+++ ichu-team-react-app/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { GiHamburgerMenu } from 'react-icons/gi'
 
 import "./App.css";
+import Home from "./components/Home/Home";
 import SocialMedia from "./components/SocialMedia/SocialMedia";
 
 function toggle() {
@@ -23,18 +24,7 @@ function App() {
           </a>
           <div id="toggle" onClick={toggle}><GiHamburgerMenu size="3em" color="#1b9945"/></div>
         </header>
-        <div className="content">
-          <h2>
-            Bienvenido a <span>Ichu team</span>
-          </h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi nemo
-            maiores nam quibusdam fugit fugiat, deleniti optio nulla quasi,
-            tempore reiciendis doloribus consequatur neque voluptate doloremque
-            adipisci, asperiores repudiandae. Quod!
-          </p>
-          <a href="/" className="btn-info">Saber mas</a>
-        </div>
+        <Home />
         <SocialMedia />
       </div>
 
